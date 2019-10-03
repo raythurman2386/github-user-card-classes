90f513cdef49d3984ce1bdb7598022a0f1ed6afa
raythurman2386 <raymondthurman5@gmail.com>
2019-10-03 19:27:55 -0400

    playing with the fetch api

--- github_cards/src/App.js
+++ github_cards/src/App.js
@@ -1,6 +1,19 @@
 import React, { Component } from 'react'
 
 class App extends Component {
+  // set up state for my card data
+  state = {
+    data: [],
+  }
+
+  // componentDidMount to fetch data
+  componentDidMount() {
+    fetch('https://api.github.com/users/raythurman2386/')
+      .then(res => res.json())
+      .then(data => this.setState({ data: data.message }))
+      .catch(err => console.log(err.response))
+  }
+
   render() {
     return (
       <div>
