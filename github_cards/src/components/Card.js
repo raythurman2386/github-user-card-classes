bb3ea19673fb545e7e3c010b60ad28d567bd236e
raythurman2386 <raymondthurman5@gmail.com>
2019-10-03 22:06:05 -0400

    created wrapper styled component

--- github_cards/src/components/Card.js
+++ github_cards/src/components/Card.js
@@ -3,7 +3,7 @@ import styled from 'styled-components'
 
 const Card = props => {
   return (
-    <div>
+    <Wrapper>
       <h1>{props.user.name}</h1>
       <img src={props.user.avatar_url} alt={props.user.name} />
       <h2>{props.user.login}</h2>
@@ -11,8 +11,24 @@ const Card = props => {
       <p>{props.user.company}</p>
       <p>{props.user.blog}</p>
       <p>{props.user.bio}</p>
-    </div>
+    </Wrapper>
   )
 }
 
 export default Card
+
+const Wrapper = styled.div`
+  width: 250px;
+  padding: 1rem;
+  margin: 1rem auto;
+  box-shadow: 0 10px 6px -6px #777;
+
+  img {
+    width: 98%;
+  }
+
+  :hover {
+    box-shadow: 0 10px 16px -6px #777;
+    transition: 0.4s ease;
+  }
+`
