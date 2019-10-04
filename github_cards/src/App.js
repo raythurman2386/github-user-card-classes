9b40f30c3b1c1fd02ad03179a113d09946a4d513
raythurman2386 <raymondthurman5@gmail.com>
2019-10-03 21:56:05 -0400

    mapping over basic follower data

--- github_cards/src/App.js
+++ github_cards/src/App.js
@@ -27,6 +27,8 @@ class App extends Component {
     return (
       <div>
         <Card user={this.state.user} />
+        {this.state.followers &&
+          this.state.followers.map(follower => <Card user={follower} />)}
       </div>
     )
   }
