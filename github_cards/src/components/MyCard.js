c32a90793f5ad05be37f96a09d2dc6b9132799e0
raythurman2386 <raymondthurman5@gmail.com>
2019-10-06 19:27:35 -0400

    added a card just for my info

--- github_cards/src/components/MyCard.js
+++ github_cards/src/components/MyCard.js
@@ -3,8 +3,13 @@ import styled from 'styled-components'
 
 const Card = props => (
   <Wrapper>
+    <h1>{props.user.name}</h1>
     <img src={props.user.avatar_url} alt={props.user.name} />
     <h2>{props.user.login}</h2>
+    <p>{props.user.location}</p>
+    <p>{props.user.company}</p>
+    <p>{props.user.blog}</p>
+    <p>{props.user.bio}</p>
   </Wrapper>
 )
 
